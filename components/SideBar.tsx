9aaeb37ecf4c87268bf0f80e01b6a505e92a4f86
BohdanBest <bestanchuk.bohdan.clg@chnu.edu.ua>
2024-02-02 01:56:09 +0200

    organized sidebar and complete search functionality

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -7,12 +7,14 @@ import { HiHome } from "react-icons/hi";
 import Box from "./Box";
 import SideBarItem from "./SideBarItem";
 import Library from "./Library";
+import { Song } from "@/types";
 
 interface Props {
   children: React.ReactNode;
+  songs: Song[];
 }
 
-const SideBar: React.FC<Props> = ({ children }) => {
+const SideBar: React.FC<Props> = ({ children, songs }) => {
   const pathname = usePathname();
   const routes = useMemo(
     () => [
@@ -42,7 +44,7 @@ const SideBar: React.FC<Props> = ({ children }) => {
           </div>
         </Box>
         <Box className="overflow-y-auto h-full">
-          <Library />
+          <Library songs={songs} />
         </Box>
       </div>
       <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["ietmdtopgkrggzxnftuk.supabase.co"],
+  },
+};
 
 export default nextConfig;
